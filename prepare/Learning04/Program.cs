62c0c3cee106f551f88430ae57da0239dd6c1504
SgtMarbear <marbear2539@gmail.com>
2024-06-06 15:17:22 -0600

    W07 Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment as1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(as1.GetSummary());
+
+        MathAssignment mAs1 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mAs1.GetSummary());
+        Console.WriteLine(mAs1.GetHomeworkList());
+
+        WritingAssignment wAs1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(wAs1.GetSummary());
+        Console.WriteLine(wAs1.GetWritingInformation());
     }
 }
