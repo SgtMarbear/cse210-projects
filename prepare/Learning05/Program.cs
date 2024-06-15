e29b23650d1fd3e85115d38406a5c0871f199d44
SgtMarbear <marbear2539@gmail.com>
2024-06-14 19:35:42 -0600

    W09 Prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Black", 5);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Grey", 6, 3);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Brown", 6);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes) {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine("The " + color + "shape has an area of " + area + ".");
+        }
     }
 }
